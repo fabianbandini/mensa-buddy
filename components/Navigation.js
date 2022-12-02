409d53a8cbd1f74ecdbbdaa8e2f3aa328a1bdea0
Fabian Bandini <91336333+fabianbandini@users.noreply.github.com>
2022-12-02 15:25:57 +0100

    add bug in dropdown

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,26 +1,78 @@
 import Image from "next/image";
-import { use, useEffect, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import styles from "./navigation.module.scss";
-import MensaApi from "../lip/api/Mensa"
+import MensaApi from "../lip/api/Mensa";
 
 export default function Navigation() {
   const [locations, setLocations] = useState([]);
+  const [locationsDisplay, setLocationsDisplay] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
   const [isDay, setIsDay] = useState(false);
   const [clicked, setClicked] = useState(false);
   const [currentLocation, setCurrentLocation] = useState("Zollikofen");
+  const [prevLocation, setPrevLocation] = useState(currentLocation);
+  //extract to .env.local
+  const displayCount = 5;
 
-  useEffect(async () => {
-    const resp = await MensaApi.getAll();
-    console.log(resp);
-  }, []) 
+  useEffect(() => {
+    const fetch = async () => {
+      const resp = await MensaApi.getAll();
+      let x = [];
 
-  const clickDropdown = () => {
-    setClicked(!clicked);
+      for (let key in resp) {
+        x.push(resp[key].name);
+      }
+
+      setLocations(x);
+    };
+
+    fetch();
+
+    window.addEventListener("click", handleDropDownState);
+
+    return () => {
+      window.removeEventListener("click", handleDropDownState);
+    }
+  }, []);
+
+  const onClickSearch = () => {
+    setPrevLocation(currentLocation);
+    setCurrentLocation("");
   };
 
-  const optionClicked = (location) => {
-    setCurrentLocation(location);
-    setClicked(false);
+  const handleDropDownState = (e) => {
+    if (
+      e.target.id != "display-list" &&
+      e.target.id != "search-input" &&
+      !e.target.classList.contains("locationContainer")
+    ) {
+      console.log("should be closed");
+      setIsSearching(false);
+      setLocationsDisplay([]);
+      
+      setPrevLocation(x => {
+        console.log(x);
+        setCurrentLocation(x)
+        return x;
+      });
+    }
+  };
+
+  useEffect(() => {
+    console.log("useEffect", currentLocation);
+  }, [currentLocation])
+
+  const search = (e) => {
+    setIsSearching(true);
+    let x = locations.filter((location) =>
+      location.includes(e.currentTarget.value)
+    );
+
+    if (x.length > displayCount) {
+      x = x.slice(0, displayCount);
+    }
+
+    setLocationsDisplay(x);
   };
 
   const clickChangeDisplayMode = (mode) => {
@@ -35,7 +87,7 @@ export default function Navigation() {
   };
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} id="lol">
       <div className={styles.left}>
         <h2>
           Mensa <span>Buddy</span>
@@ -54,33 +106,28 @@ export default function Navigation() {
             onClick={() => clickChangeDisplayMode("week")}
           />
         </div>
-        <div className={styles.dropdown}>
-          <span
-            className={
-              !clicked
-                ? styles.current
-                : styles.current + " " + styles.currentActive
-            }
-            onClick={clickDropdown}
-          >
-            {currentLocation}
-            <div className={styles.arrow}></div>
-          </span>
-          <div
-            className={
-              !clicked
-                ? styles.options + " " + styles.optionsInactive
-                : styles.options + " " + styles.optionsActive
-            }
-          >
-            {locations.map((location) => {
+        {/*todo: overrengineer searchbar...*/}
+        <div className={styles.searchbar}>
+          <input
+            defaultValue={currentLocation}
+            onChange={(e) => search(e)}
+            style={{
+              borderBottomRightRadius: isSearching ? "0px" : "5px",
+              borderBottomLeftRadius: isSearching ? "0px" : "5px",
+            }}
+            onClick={onClickSearch}
+            id="search-input"
+          />
+          <div className={styles.displayList} id="display-list">
+            {locationsDisplay.map((location) => {
+              //add key to p
               return (
-                <span
-                  className={styles.option}
-                  onClick={() => optionClicked(location)}
+                <div
+                  className={`${styles.locationContainer} locationContainer`}
+                  key={location}
                 >
                   {location}
-                </span>
+                </div>
               );
             })}
           </div>
